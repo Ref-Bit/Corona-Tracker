1de02a1c0588b924020b2ede749d1d2dde4df68c
Ref-Bit <rsv.gho@gmail.com>
2020-04-12 17:49:46 +0300

    Ready to launch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Cards, Chart, CountryPicker } from './components'
+import Styles from './App.module.css';
+import { fetchData } from './API';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    data: {},
+    country: ''
+  }
+  async componentWillMount(){
+    const fetchedData = await fetchData();
+    this.setState({data: fetchedData});
+  }
+  handleCountryChange = async (country) => {
+    // Fetch Data
+    const fetchedData = await fetchData(country);
+
+    // Set Country
+    this.setState({ data:fetchedData, country: country });
+  }
+  render(){
+    const { data, country } = this.state
+    return(
+      <div className={Styles.container}>
+        <img src="logo.png" alt="Logo" className={Styles.logo}/>
+        <Cards data={data}/>
+        <CountryPicker handleCountryChange={this.handleCountryChange}/>
+        <Chart data={data} country={country}/>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import Styles from './Cards.module.css';
+import CountUp from 'react-countup';
+import CX from 'classnames';
+
+const Cards = ( {data:{ confirmed, recovered, deaths, lastUpdate }}) => {
+  if(!confirmed){
+    return 'Loading...';
+  }
+  return (
+    <div className={Styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card} xs={12} md={3} className={CX(Styles.card, Styles.infected)}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>Infected</Typography>
+            <Typography variant="h5">
+              <CountUp 
+                start={0}
+                end={confirmed.value}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+            <Typography variant="body2">Number of active cases of COVID-19</Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card} xs={12} md={3} className={CX(Styles.card, Styles.recovered)}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+            <Typography variant="h5">
+              <CountUp 
+                start={0}
+                end={recovered.value}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+            <Typography variant="body2">Number of reovered cases of COVID-19</Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card} xs={12} md={3} className={CX(Styles.card, Styles.deaths)}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+            <Typography variant="h5">
+              <CountUp 
+                start={0}
+                end={deaths.value}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+            <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+
+export default Cards;
