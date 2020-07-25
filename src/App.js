5396a2539d10fb7e7aa239be9e97a31a3651a684
Ref-Bit <rsv.gho@gmail.com>
2020-07-25 09:58:22 +0300

    Updated language switcher

--- src/App.js
+++ src/App.js
@@ -1,75 +1,92 @@
-import React from 'react';
-import axios from 'axios';
-import moment from 'moment';
-import { withTranslation } from 'react-i18next';
-import { ThemeProvider } from '@material-ui/core/styles'
-import { Cards, Chart, CountryPicker } from './components'
-import { theme, dev } from './config';
-import { fetchData } from './api';
-import Lang from './switchLanguage'
-import Styles from './App.module.css';
+import React from "react";
+import axios from "axios";
+import { withTranslation } from "react-i18next";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { Cards, Chart, CountryPicker } from "./components";
+import { theme, dev } from "./config";
+import { fetchData } from "./api";
+import { switchLang } from "./helpers";
+import Styles from "./App.module.css";
 
-class App extends React.Component{
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       data: {},
-      country: '',
-      code:'',
-      flag: ''
-    }
-  } 
-  async componentDidMount(){
+      country: "",
+      code: "",
+      flag: "",
+    };
+  }
+  async componentDidMount() {
     const fetchedData = await fetchData();
-    this.setState({data: fetchedData});
+    this.setState({ data: fetchedData });
     this.fetchIP();
   }
   fetchIP = () => {
-    axios.get('https://freegeoip.app/json/')
-    .then((res) => {
-      const { country_code } = res.data;
-      axios.get(`https://restcountries.eu/rest/v2/alpha/${country_code}`).then((res) => {
-        const code = res.data.languages[0].iso639_1
-        const { flag } = res.data;
-        this.setState({code})
-        this.setState({flag})
+    axios
+      .get("https://freegeoip.app/json/")
+      .then((res) => {
+        const { country_code } = res.data;
+        axios
+          .get(`https://restcountries.eu/rest/v2/alpha/${country_code}`)
+          .then((res) => {
+            const code = res.data.languages[0].iso639_1;
+            const { flag } = res.data;
+            this.setState({ code });
+            this.setState({ flag });
 
-        Lang.prototype.switchLang(code);
-
-      }).catch((err) => console.log(err))
-    })      
-    .catch((err) => console.log(err));
-  } 
+            switchLang(code);
+          })
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+  };
   handleCountryChange = async (country) => {
     // Fetch Data
     const fetchedData = await fetchData(country);
 
     // Set Country
-    this.setState({ data:fetchedData, country: country });
-  }
-  
-  render(){
+    this.setState({ data: fetchedData, country: country });
+  };
+
+  render() {
     const { data, country, code } = this.state;
     // const { t } = this.props;
 
-    return(
+    return (
       <ThemeProvider theme={theme}>
         <div className={Styles.container}>
-          <img src="logo.png" alt="Logo" className={Styles.logo}/>
-          <Cards data={data} locale={code}/>
-          <CountryPicker handleCountryChange={this.handleCountryChange}/>
-          <Chart data={data} country={country}/>
-          <p>This web app uses <span>Covid19-Mathdro API</span> to update coronavirus stats.</p>
+          <img src="logo.png" alt="Logo" className={Styles.logo} />
+          <Cards data={data} locale={code} />
+          <CountryPicker handleCountryChange={this.handleCountryChange} />
+          <Chart data={data} country={country} />
+          <p>
+            This data is provided by <span>Covid19-Mathdro API</span>
+          </p>
           <div className={Styles.copyright}>
-            <h5>This project was inspired by&nbsp; 
-              <a href={dev.origin.repo} target="_blank" rel="noopener noreferrer">{dev.origin.name}</a> 
-              &nbsp;and built by  
-              <a href={dev.developed.repo} target="_blank" rel="noopener noreferrer">{dev.developed.name}</a>
+            <h5>
+              This project was inspired by&nbsp;
+              <a
+                href={dev.origin.repo}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {dev.origin.name}
+              </a>
+              &nbsp;and built by&nbsp;
+              <a
+                href={dev.developed.repo}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {dev.developed.name}
+              </a>
             </h5>
           </div>
         </div>
       </ThemeProvider>
-    )
+    );
   }
 }
 
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,69 +1,75 @@
-import React, { useState, useEffect } from 'react';
-import i18next from 'i18next';
-import { fetchDailyData } from '../../api';
-import { Line, Bar } from 'react-chartjs-2';
-import Styles from './Chart.module.css';
-import '../../config';
+import React, { useState, useEffect } from "react";
+import i18next from "i18next";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
+import Styles from "./Chart.module.css";
+import "../../config";
 
-const Chart = ({ data: {confirmed, recovered, deaths}, country }) => {
-  const [ dailyData, setDailyData ] = useState([]);
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
+  const [dailyData, setDailyData] = useState([]);
   useEffect(() => {
     const fetchAPI = async () => {
       setDailyData(await fetchDailyData());
-    }
-    
+    };
+
     fetchAPI();
   }, []);
-  const lineChart = (
-    dailyData.length ? (
-      <Line 
-        data={{
-          labels: dailyData.map(({ date }) => date),
-          datasets: [{
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
             data: dailyData.map(({ confirmed }) => confirmed),
-            label: i18next.t('infected.title'),
-            borderColor: '#3333ff',
-            fill: true
-          }, {
+            label: i18next.t("infected.title"),
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
             data: dailyData.map(({ deaths }) => deaths),
-            label: i18next.t('deaths.title'),
-            borderColor: 'red',
-            backgroundColor: 'rgba(255, 0, 0, 0.5)',
-            fill: true
-          }],
-        }}
-      />) : null
-  );
-  
-  const barChart = (
-    confirmed 
-    ? (
-      <Bar
-        data={{
-          labels: [i18next.t('infected.title'), i18next.t('recovered.title'), i18next.t('deaths.title')],
-          datasets: [{
-            label: i18next.t('labels.people'),
+            label: i18next.t("deaths.title"),
+            borderColor: "red",
+            backgroundColor: "rgba(255, 0, 0, 0.5)",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
+
+  const barChart = confirmed ? (
+    <Bar
+      data={{
+        labels: [
+          i18next.t("infected.title"),
+          i18next.t("recovered.title"),
+          i18next.t("deaths.title"),
+        ],
+        datasets: [
+          {
+            label: i18next.t("labels.people"),
             backgroundColor: [
-              'rgba(0, 0, 255, 0.5)',
-              'rgba(0, 255, 0, 0.5)',
-              'rgba(255, 0, 0, 0.5)',
+              "rgba(0, 0, 255, 0.5)",
+              "rgba(0, 255, 0, 0.5)",
+              "rgba(255, 0, 0, 0.5)",
             ],
-            data: [confirmed.value, recovered.value, deaths.value] 
-          }]
-        }}
-        options={{
-          legend: { display: false },
-          title: { display: true, text: [i18next.t('desc.country'), i18next.t(country)] },
-        }} 
-      />
-    ) : null
-  )
+            data: [confirmed.value, recovered.value, deaths.value],
+          },
+        ],
+      }}
+      options={{
+        legend: { display: false },
+        title: {
+          display: true,
+          text: [i18next.t("desc.country"), i18next.t(country)],
+        },
+      }}
+    />
+  ) : null;
 
   return (
-    <div className={Styles.container}>
-      {country ? barChart : lineChart}
-    </div>
-  )
-}
+    <div className={Styles.container}>{country ? barChart : lineChart}</div>
+  );
+};
 
 export default Chart;
